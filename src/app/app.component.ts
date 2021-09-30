17f36d541d5b4601841a3e567741bf7ee5a4d6cc
SR.bala <sr.bala.kkl@gmail.com>
2021-09-30 19:30:16 +0530

    use dom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EventBinding';
+
+  mytext:string="";
+
+  onType(event : Event){
+    console.log(event);
+    console.log("asdf");
+
+    this.mytext = (<HTMLInputElement>event.target).value;
+  }
+
+  onclick(){
+    console.log("clicked");
+  }
 }
